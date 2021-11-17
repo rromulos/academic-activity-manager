93354b8731526b350e0a553f1edf2cf7c3c99b1d
Rômulo von Santos <rromulosp@gmail.com>
2021-11-16 23:48:26 -0300

    Adding columns to the Activity.

--- app/Http/Controllers/Admin/ActivityCrudController.php
+++ app/Http/Controllers/Admin/ActivityCrudController.php
@@ -21,7 +21,7 @@ class ActivityCrudController extends CrudController
 
     /**
      * Configure the CrudPanel object. Apply settings to all operations.
-     * 
+     *
      * @return void
      */
     public function setup()
@@ -33,24 +33,71 @@ class ActivityCrudController extends CrudController
 
     /**
      * Define what happens when the List operation is loaded.
-     * 
+     *
      * @see  https://backpackforlaravel.com/docs/crud-operation-list-entries
      * @return void
      */
     protected function setupListOperation()
     {
-        
-
-        /**
-         * Columns can be defined using the fluent syntax or array syntax:
-         * - CRUD::column('price')->type('number');
-         * - CRUD::addColumn(['name' => 'price', 'type' => 'number']); 
-         */
+        CRUD::addColumn([
+            'name'  => 'id',
+            'label' => trans('backpack::crud.Id'),
+            'type'  => 'text',
+            'priority' => 1,
+        ]);
+        CRUD::addColumn([
+            'label' => trans('backpack::crud.Student'),
+            'type' => 'select',
+            'name' => 'student_id',
+            'entity' => 'Student',
+            'attribute' => 'name',
+            'priority' => 2,
+        ]);
+        CRUD::addColumn([
+            'label' => trans('backpack::crud.Subject'),
+            'type' => 'select',
+            'name' => 'subject_id',
+            'entity' => 'Subject',
+            'attribute' => 'name',
+            'priority' => 3,
+        ]);
+        CRUD::addColumn([
+            'name'  => 'type',
+            'label' => trans('backpack::crud.Type'),
+            'type'  => 'text',
+            'priority' => 4,
+        ]);
+        CRUD::addColumn([
+            'name' => 'delivery_date',
+            'label' => trans('backpack::crud.Subject'),
+            "type" => "date",
+            'format' => 'l',
+            'priority' => 5
+        ]);
+        CRUD::addColumn([
+            'name'  => 'observation',
+            'label' => trans('backpack::crud.Observation'),
+            'type'  => 'text',
+            'priority' => 6,
+        ]);
+        CRUD::addColumn([
+            'name' => 'price',
+            'label' => trans('backpack::crud.Price'),
+            'type' => 'text',
+            'prefix' => 'R$',
+            'priority' => 7
+        ]);
+        CRUD::addColumn([
+            'name'  => 'status',
+            'label' => trans('backpack::crud.Status'),
+            'type'  => 'text',
+            'priority' => 8,
+        ]);
     }
 
     /**
      * Define what happens when the Create operation is loaded.
-     * 
+     *
      * @see https://backpackforlaravel.com/docs/crud-operation-create
      * @return void
      */
@@ -58,18 +105,18 @@ class ActivityCrudController extends CrudController
     {
         CRUD::setValidation(ActivityRequest::class);
 
-        
+
 
         /**
          * Fields can be defined using the fluent syntax or array syntax:
          * - CRUD::field('price')->type('number');
-         * - CRUD::addField(['name' => 'price', 'type' => 'number'])); 
+         * - CRUD::addField(['name' => 'price', 'type' => 'number']));
          */
     }
 
     /**
      * Define what happens when the Update operation is loaded.
-     * 
+     *
      * @see https://backpackforlaravel.com/docs/crud-operation-update
      * @return void
      */
